5f527674e18ed430aae134df3879f71e173f1491
akaur79 <akaur79@ucsc.edu>
2024-04-05 21:40:35 -0700

    added to /looking and /offering

---
+++ hackathon2024/src/app/looking/page.js
@@ -0,0 +1,5 @@
+export default function Looking() {
+    return (
+        <div>Looking</div>
+        );
+}
---
+++ hackathon2024/src/app/offering/page.js
@@ -0,0 +1,10 @@
+export default function Offering() {
+    return (
+        <div>
+            <div>Fill out some info!</div>
+            <div># of Parking spots</div>
+            <div>Images</div>
+            <a href="/" className="nav-link">Enter</a>
+        </div>
+    );
+}
