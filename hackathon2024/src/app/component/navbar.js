f67c3b650a0ad64317e7457f725a23c5bb49e9cd
JDLiang100 <juandavidliang@gmail.com>
2024-04-05 23:33:07 -0700

    installed boostrap

--- hackathon2024/src/app/component/navbar.js
+++ hackathon2024/src/app/component/navbar.js
@@ -1,22 +1,9 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Navbar() {
   return (
-<<<<<<< HEAD
-    <nav className="navbar">
-      <div className="left-links">
-        <a href="#" className="nav-link">
-          <img src="/car.png"/>
-        </a>
-      </div>
-      <div className="right-links">
-        <a href="/signup" className="nav-link">Sign Up</a>
-        <a href="/login" className="nav-link">Log In</a>
-      </div>
-    </nav>
-=======
     <div>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -40,7 +27,6 @@ function Navbar() {
       <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </div>
->>>>>>> decbd734ecd7778d3a5eeceee4d98d34d82a8082
   );
 }
 
