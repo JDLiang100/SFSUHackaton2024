3fc707400e18174755489f57a6b4c396f53b7559
JDLiang100 <juandavidliang@gmail.com>
2024-04-05 21:16:35 -0700

    made basic template for navbar

--- hackathon2024/src/app/page.js
+++ hackathon2024/src/app/page.js
@@ -1,17 +1,8 @@
+import Navbar from './component/navbar';
 export default function Home() {
   return (
     <main>
-       <div className="App">
-        <nav className="navbar">
-          <div className="left-links">
-            <a href="/" className="nav-link">Home</a>
-          </div>
-          <div className="right-links">
-            <a href="/signup" className="nav-link">Sign Up</a>
-            <a href="/login" className="nav-link">Log In</a>
-          </div>
-        </nav>
-      </div>
+      <Navbar />
       <h1>Find Parking!</h1>
       <div>
         <input placeholder="Your address"></input>
