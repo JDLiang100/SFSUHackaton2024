146cb86eb1c04306f60547ee5307492a189acc6f
akaur79 <akaur79@ucsc.edu>
2024-04-06 10:57:48 -0700

    adding material ui

--- hackathon2024/src/app/page.js
+++ hackathon2024/src/app/page.js
@@ -1,5 +1,6 @@
 import Navbar from './component/navbar';
 import Link from 'next/link';
+import TextField from '@mui/material/TextField';
 export default function Home() {
   return (
     <main>
@@ -11,10 +12,11 @@ export default function Home() {
               <h1 className="text-3xl font-bold">Find Parking!</h1>
             </div>
             <div className="mb-8">
-              <input
+            <TextField
+                label="Your address"
+                variant="outlined"
                 type="text"
-                placeholder="Your address"
-                className="w-full border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:border-indigo-500"
+                className="w-full"
               />
             </div>
             <div className="flex justify-between">
