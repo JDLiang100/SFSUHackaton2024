decbd734ecd7778d3a5eeceee4d98d34d82a8082
akaur79 <akaur79@ucsc.edu>
2024-04-05 23:15:24 -0700

    updated offering

--- hackathon2024/src/app/offering/page.js
+++ hackathon2024/src/app/offering/page.js
@@ -4,6 +4,8 @@ export default function Offering() {
             <div>Fill out some info!</div>
             <div># of Parking spots</div>
             <div>Images</div>
+            <div>Price per space</div>
+            
             <a href="/" className="nav-link">Enter</a>
         </div>
     );
