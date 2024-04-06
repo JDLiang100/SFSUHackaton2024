f2722d99e803f63725e633402f20829e40e10a17
Miguel Logarta <maclogarta@gmail.com>
2024-04-05 23:54:22 -0700

    navbar classnames

--- hackathon2024/src/app/component/navbar.js
+++ hackathon2024/src/app/component/navbar.js
@@ -4,20 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Navbar() {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="/">Home<span class="sr-only">(current)</span></a>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="/">Home<span className="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/signup">Sign Up</a>
+            <li className="nav-item">
+              <a className="nav-link" href="/signup">Sign Up</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/login">Login</a>
+            <li className="nav-item">
+              <a className="nav-link" href="/login">Login</a>
             </li>
 
           </ul>
