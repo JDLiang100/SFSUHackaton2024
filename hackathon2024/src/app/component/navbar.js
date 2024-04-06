da42c9602b4ddf9ceb2e05ab0641ac124e7e2e16
JDLiang100 <juandavidliang@gmail.com>
2024-04-06 00:22:38 -0700

    fixed navbar with tailwind

--- hackathon2024/src/app/component/navbar.js
+++ hackathon2024/src/app/component/navbar.js
@@ -1,32 +1,19 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function Navbar() {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="/">Home<span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/signup">Sign Up</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/login">Login</a>
-            </li>
-
-          </ul>
-        </div>
-      </nav>
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    </div>
+    <nav className="navbar">
+      <div className="left-links">
+        <a href="/" className="home-link">
+          Home
+        </a>
+      </div>
+      <div className="right-links">
+        <a href="/signup" className="nav-link">Sign Up</a>
+        <a href="/login" className="nav-link">Log In</a>
+      </div>
+    </nav>
   );
 }
 
--- hackathon2024/src/app/login/page.js
+++ hackathon2024/src/app/login/page.js
@@ -1,22 +1,8 @@
 import Navbar from "../component/navbar"
 export default function Login() {
     return (
-        
         <div>
-            <Navbar/>
-            <form class="info-form">
-                <h1>Log In</h1>
-                <div class="form-group">
-                    <label >Email address</label>
-                    <input type="email" class="form-control" id="email" placeholder="Enter email"/>
-                </div>
-                <div class="form-group">
-                    <label >Password</label>
-                    <input type="password" class="form-control" id="password1" placeholder="Password"/>
-                </div>
-
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+            <Navbar />
         </div>
         );
 }
