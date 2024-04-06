df8c1c5bfbb0237cdebc7469e58ebda483a4a5cc
JDLiang100 <juandavidliang@gmail.com>
2024-04-06 00:28:56 -0700

    home test

--- hackathon2024/src/app/page.js
+++ hackathon2024/src/app/page.js
@@ -1,16 +1,25 @@
 import Navbar from './component/navbar';
+
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <h1>Find Parking!</h1>
-      <div>
-        <input placeholder="Your address"></input>
-      </div>
-      <div>
-      <a href="/looking" className="nav-link">Looking</a>
-      <a href="/offering" className="nav-link">Offering</a>
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="max-w-md w-full bg-white p-8 rounded-lg shadow-lg">
+          <h1 className="text-2xl font-bold mb-4">Find Parking!</h1>
+          <div className="mb-4">
+            <input
+              type="text"
+              placeholder="Your address"
+              className="w-full border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:border-indigo-500"
+            />
+          </div>
+          <div className="flex justify-between">
+            <a href="/looking" className="text-indigo-500 hover:text-indigo-600">Looking</a>
+            <a href="/offering" className="text-indigo-500 hover:text-indigo-600">Offering</a>
+          </div>
+        </div>
       </div>
     </main>
-  )
+  );
 }
