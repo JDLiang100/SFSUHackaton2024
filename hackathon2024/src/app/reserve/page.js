bd5003c0e03d4b68f8b9763969bebd3358a21a12
akaur79 <akaur79@ucsc.edu>
2024-04-07 10:33:12 -0700

    added header

--- hackathon2024/src/app/reserve/page.js
+++ hackathon2024/src/app/reserve/page.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import Navbar from '../component/navbar';
 
 const ReservePage = () => {
   return (
-    <div className="reserve-page-container">
-      <h1 className="reserve-page-title">Congratulations!</h1>
-      <p className="reserve-page-message">
-        You have successfully reserved your first driveway. We appreciate your business and look forward to providing you with excellent service.
-      </p>
+    <div>
+      <Navbar />
+      <div className="reserve-page-container">
+        <h1 className="reserve-page-title">Congratulations!</h1>
+        <p className="reserve-page-message">
+          You have successfully reserved your first driveway. We appreciate your business and look forward to providing you with excellent service.
+        </p>
+      </div>
     </div>
   );
 };
+
 
 export default ReservePage;
