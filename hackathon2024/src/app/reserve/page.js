2bb589c4b3d69310150226582b392b2bdbddb377
akaur79 <akaur79@ucsc.edu>
2024-04-07 10:46:03 -0700

    centered it

--- hackathon2024/src/app/reserve/page.js
+++ hackathon2024/src/app/reserve/page.js
@@ -6,14 +6,13 @@ const ReservePage = () => {
     <div>
       <Navbar />
       <div className="reserve-page-container">
-        <h1 className="reserve-page-title">Congratulations!</h1>
-        <p className="reserve-page-message">
+        <h1 className="reserve-page-title text-center mb-4">Congratulations!</h1>
+        <p className="reserve-page-message text-center">
           You have successfully reserved your first driveway. We appreciate your business and look forward to providing you with excellent service.
         </p>
       </div>
     </div>
   );
 };
-
 
 export default ReservePage;
